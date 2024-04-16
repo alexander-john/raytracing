c927272c61d01ccbda74b7ba353a0f4fc9cdd438
Alexander John <alexjohnpersonals@gmail.com>
2024-04-15 21:34:07 -0700

    first commit

---
+++ build/CMakeFiles/raytracing.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracing.dir/main.cpp.obj"
+  "CMakeFiles/raytracing.dir/main.cpp.obj.d"
+  "libraytracing.dll.a"
+  "raytracing.exe"
+  "raytracing.exe.manifest"
+  "raytracing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Projects/raytracing
+# Build directory: C:/Projects/raytracing/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
